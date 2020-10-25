6e022684df9034b8d37a8c43ba53a2594ce8f2af
Joel Barnes <j@barnzilla.ca>
2020-10-24 20:08:59 -0400

    Rebuild package and make minor tweaks to how package is built

--- create-glebe-datasets-package.R
+++ create-glebe-datasets-package.R
@@ -1,15 +1,15 @@
 # Load packages
-package_names <- c("devtools", "NCmisc", "klippy", "roxygen2")
+package_names <- c("devtools", "NCmisc", "roxygen2")
 load_packages <- lapply(package_names, require, character.only = TRUE)
 
 # Create package
-create(path = "c:/users/joel/google drive/github/glebe.datasets")
+create(path = "c:/users/joelb/google drive/github/glebe.datasets")
 
 # Customize the DESCRIPTION file
 use_description(fields = list(
   Language = "en",
   Title = "Create demo datasets for instructional use",
-  Version = "0.5",
+  Version = "0.6",
   Description = "This package contains tools to create demo datasets for instructional use (e.g., in undergraduate statistics classes).",
   `Authors@R` = c(
     person(given = "Joel", family = "Barnes", email = "j@barnzilla.ca", role = c("aut", "cre"), comment = c(ORCID = "0000-0002-7621-9020")),
@@ -28,12 +28,13 @@ use_gpl3_license(name = "Joel Barnes; Michelle Guerrero")
 
 # Add dependencies to the DESCRIPTION file
 use_package("haven", "Imports")
+use_package("lqmm", "Imports")
 use_package("scales", "Imports")
 use_package("simstudy", "Imports")
 use_package("writexl", "Imports")
 
 # Add suggested packages to the DESCRIPTION file
-use_package("klippy", "Suggests")
+#use_package("klippy", "Suggests")
 use_package("knitr", "Suggests")
 use_package("rmarkdown", "Suggests")
 
@@ -41,13 +42,14 @@ use_package("rmarkdown", "Suggests")
 document()
 
 # Find all functions by package for a given file
-package_names <- c("haven", "scales", "simstudy", "writexl")
+package_names <- c("haven", "lqmm", "scales", "simstudy", "writexl")
 load_packages <- lapply(package_names, require, character.only = TRUE)
 functions1 <- list.functions.in.file("c:/users/joel/google drive/github/glebe.datasets/r/utilities.R")
 functions2 <- list.functions.in.file("c:/users/joel/google drive/github/glebe.datasets/r/create_dataset.R")
 
-# Create CAPL demo data
-use_data(capl_demo_data, overwrite = TRUE)
+# Create demo data
+dataset1 <- get_dataset()
+use_data(dataset1, overwrite = TRUE)
 
 # Run a check on the package
 check()
@@ -56,7 +58,7 @@ check()
 build_vignettes()
 
 # Build package
-build("c:/users/joel/google drive/github/glebe.datasets", path = "c:/users/joel/google drive/github/r-package-sources")
+build("c:/users/joelb/google drive/github/glebe.datasets", path = "c:/users/joelb/google drive/github/r-package-sources")
 
 # Build PDF manual
 #tinytex::install_tinytex()
